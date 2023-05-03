6bbe0838a9f9d24d2c825e436649166230551155
N3rdy <n3rdydzn@hotmail.com>
2023-05-03 10:08:28 -0300

    1 Done

--- AppXamarinFlix/AppXamarinFlix/Categorias/Aventura.xaml.cs
+++ AppXamarinFlix/AppXamarinFlix/Categorias/Aventura.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppXamarinFlix.Filmes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,8 +16,21 @@ namespace AppXamarinFlix.Categorias
         public Aventura()
         {
             InitializeComponent();
-            NavigationPage.SetHasNavigationBar(this, false);
-            logo.Source = ImageSource.FromResource("AppXamarinFlix.Img.logo.png");
+                NavigationPage.SetHasNavigationBar(this, false);
+                logo.Source = ImageSource.FromResource("AppXamarinFlix.Img.logo.png");
+                btnGatoDeBotas.Source = ImageSource.FromResource("AppXamarinFlix.Posters.gatodebotas.jpg");
+        }
+
+        private void btnGatoDeBotas_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new GatoDeBotas2());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "Ok");
+            }
         }
     }
 }
--- AppXamarinFlix/AppXamarinFlix/Filmes/GatoDeBotas2.xaml.cs
+++ AppXamarinFlix/AppXamarinFlix/Filmes/GatoDeBotas2.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,13 @@ namespace AppXamarinFlix.Filmes
         public GatoDeBotas2()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            logo.Source = ImageSource.FromResource("AppXamarinFlix.Img.logo.png");
+            poster.Source = ImageSource.FromResource("AppXamarinFlix.Posters.gatodebotas.jpg");
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.Html = @"<iframe width='400' height='300' frameborder='0' src='https://www.youtube.com/embed/QAcn7cWm_hc?controls=0' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' allowfullscreen></iframe>";
+            visualizador.Source = htmlSource;
+            sinopse.Text = "O Gato de Botas descobre que sua paixão pela aventura cobrou seu preço: ele já gastou oito de suas nove vidas. Ele então parte em uma jornada épica para encontrar o mítico Último Desejo e restaurar suas nove vidas.";
         }
     }
 }
